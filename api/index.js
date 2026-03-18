a7b2db48ae36eaa55e31dd9cfe9dc61add0825cd
Abhay Parmar <parmarabhay820@gmail.com>
2026-03-18 20:27:31 +0530

    Final cleanup: Removed phantom 'app' folder

--- api/index.js
+++
@@ -1,7 +0,0 @@
-// Entry point for Vercel Serverless Functions
-const app = require('../Backend/server');
-
-// This handler ensures that Express receives the full request details
-module.exports = (req, res) => {
-    return app(req, res);
-};
